1fd71aa6e6ae079df9b63ce7b5adf1664da30b27
Henrique Márcio <henriquemarcio0081@gmail.com>
2022-09-22 13:32:27 +0000

    tentativa do contaTreino

--- Gym/main.java
+++ Gym/main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         
         cliente c1 = new cliente("caio", "123", 22, 3251);
         treino t1 = new treino();
-        treino.contaTreino(1);
+
+        treino.contaTreino();
     }
 }
--- Gym/treino.java
+++ Gym/treino.java
@@ -1,12 +1,17 @@
 package Gym;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class treino {
-    private String[] treinos = { "a", "b", "c", "d", "e" };
-    private int[] cont = new int[5];
+    protected String[] treinos = { "a", "b", "c", "d", "e" };
+    protected int[] cont = new int[5];
 
-    public void contaTreino(int escolha) {
+    public void contaTreino() {
+        Scanner sc = new Scanner(System.in);
+        int escolha;
+        System.out.println("INFORME O TREINO DESEJADO: 1, 2, 3, 4 ou 5");
+        escolha = sc.nextInt();
 
         do {
             switch (escolha) {
@@ -25,17 +30,21 @@ public class treino {
                 case 5:
                     cont[5]++;
                     break;
+                case 0:
+                    System.out.println("saiu");
+                    break;
                 default:
-                    System.out.println("Treino não aceito");
+                    System.out.println("Treino não aceito, informe novamente: ");
 
             }
-        } while (escolha < 1 && escolha > 5);
+        } while (escolha != 0);
     }
 
     @Override
     public String toString() {
-        return "Treinos /n treino " + treinos[1] + ": " + cont[1] + "\n treino " + treinos[2] + ": " + cont[2] + "\n treino " + treinos[3] + ": " + cont[3] + "\n treino " + treinos[4] + ": " + cont[4] + "\n treino " + treinos[5] + ": " + cont[5];
+        return "Treinos /n treino " + treinos[1] + ": " + cont[1] + "\n treino " + treinos[2] + ": " + cont[2]
+                + "\n treino " + treinos[3] + ": " + cont[3] + "\n treino " + treinos[4] + ": " + cont[4] + "\n treino "
+                + treinos[5] + ": " + cont[5];
     }
 
-    
 }
