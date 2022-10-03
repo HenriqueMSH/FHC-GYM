170873429e69150fc770ab0029e2260d0c5a0ff2
FilipeGranja <filipe.00000847940@unicap.br>
2022-10-03 09:31:35 -0300

    treino.contaTreino(); comentado na main

--- Gym/main.java
+++ Gym/main.java
@@ -8,7 +8,7 @@ public class main {
         cliente c1 = new cliente("caio", "123", 22, 3251);
         treino t1 = new treino();
 
-        treino.contaTreino();
+        //treino.contaTreino();
 
     }
 }
