31125c681b5bcdfd60bc3b3ef5cfb4f068ebe02c
henriquemsh <henriquemarcio0081@gmail.com>
2022-10-13 10:39:53 -0300

    inicio de exception

--- Gym/treino.java
+++ Gym/treino.java
@@ -9,41 +9,45 @@ public class treino {
     private double valor;
     private int estoque;
 
-     
     protected String[] treinos = { "a", "b", "c", "d", "e" };
     protected static int[] cont = new int[5];
 
-    public static void contaTreino() {
+    public static void contaTreino() throws MuitaGenteException {
         Scanner sc = new Scanner(System.in);
         int escolha;
-        
-        do {
-            System.out.println("INFORME O TREINO DESEJADO: \n 1 'A - Peito', \n 2 'B - Costas, \n 3 'C - Abdômen, \n 4 'D - Perna \n 5 'E - Ombro");
-            escolha = sc.nextInt();
-            switch (escolha) {
-                case 1:
-                    cont[0]++;
-                    break;
-                case 2:
-                    cont[1]++;
-                    break;
-                case 3:
-                    cont[2]++;
-                    break;
-                case 4:
-                    cont[3]++;
-                    break;
-                case 5:
-                    cont[4]++;
-                    break;
-                case 0:
-                    System.out.println("saiu");
-                    break;
-                default:
-                    System.out.println("Treino não aceito, informe novamente: ");
 
-            }
-        } while (escolha != 0);
+        if (cont[0] > 100 || cont[1] > 100 || cont[2] > 100 || cont[3] > 100 || cont[4] > 100) {
+            throw new MuitaGenteException("Mais de 100 pessoa no treino");
+        } else {
+            do {
+                System.out.println(
+                        "INFORME O TREINO DESEJADO: \n 1 'A - Peito, \n 2 'B - Costas, \n 3 'C - Abdômen, \n 4 'D - Perna \n 5 'E - Ombro");
+                escolha = sc.nextInt();
+                switch (escolha) {
+                    case 1:
+                        cont[0]++;
+                        break;
+                    case 2:
+                        cont[1]++;
+                        break;
+                    case 3:
+                        cont[2]++;
+                        break;
+                    case 4:
+                        cont[3]++;
+                        break;
+                    case 5:
+                        cont[4]++;
+                        break;
+                    case 0:
+                        System.out.println("saiu");
+                        break;
+                    default:
+                        System.out.println("Treino não aceito, informe novamente: ");
+
+                }
+            } while (escolha != 0);
+        }
     }
 
     @Override
